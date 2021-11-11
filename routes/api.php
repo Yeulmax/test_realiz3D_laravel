5f99155191fa312a007cd2ae3623c95e85c8c887
Yeulmax <pierre.mayeul@hotmail.com>
2021-11-11 17:21:47 +0100

    Web and Api routes

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,9 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\LotController;
+use App\Http\Controllers\GroupController;
+use App\Http\Controllers\GroupTypeController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,4 +19,8 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::middleware('api')->group(function () {
+    Route::resource('products', ProductController::class);
 });
